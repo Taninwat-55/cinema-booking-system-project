6cb000fe9385d33b07c5c8404849de2bdd521db3
Valmir10 <valmir8@hotmail.se>
2025-04-11 21:45:22 +0200

    update LandingPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { Outlet } from 'react-router-dom';
-import './styles/App.css';
-import Navbar from './components/Navbar';
+import { Outlet } from "react-router-dom";
+import "./styles/App.css";
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-      <nav>
-        <Navbar />
-      </nav>
-      <main>
-        <Outlet />
-      </main>
+      {/* <nav><Navbar /></nav> */}
+      {/* <main> */}
+      <Outlet />
+      {/* </main> */}
     </>
   );
 }
