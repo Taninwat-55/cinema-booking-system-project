3eda465108a3777e9c0b367ae2ef36d1b405a526
Milad-Shirvan <miladmagic3@gmail.com>
2025-04-22 11:25:43 +0200

    small fixes

--- client/src/pages/admin/AdminAddShowingPage.jsx
+++ client/src/pages/admin/AdminAddShowingPage.jsx
@@ -16,7 +16,6 @@ const AdminAddShowingPage = () => {
     price_child: 80,
     price_senior: 100,
   });
-  const [message, setMessage] = useState(null);
 
   useEffect(() => {
     fetch('http://localhost:3001/api/movies')
@@ -58,84 +57,79 @@ const AdminAddShowingPage = () => {
 
   return (
     <>
-    <Navbar /> 
-    <div className="admin-add-showing-container">
-      <h1 className="admin-title">Add New Showing</h1>
-      {message && (
-        <div className={`message-box ${message.type}`}>
-          {message.text}
-        </div>
-      )}
-      <form className="admin-form" onSubmit={handleSubmit}>
-        <select
-          name="movie_id"
-          value={showing.movie_id}
-          onChange={handleChange}
-          className="admin-select"
-          required
-        >
-          <option value="">Select Movie</option>
-          {movies.map((movie) => (
-            <option key={movie.movie_id} value={movie.movie_id}>
-              {movie.title}
-            </option>
-          ))}
-        </select>
-        <select
-          name="theater_id"
-          value={showing.theater_id}
-          onChange={handleChange}
-          className="admin-select"
-          required
-        >
-          <option value={1}>Theater 1</option>
-          <option value={2}>Theater 2</option>
-        </select>
-        <input
-          type="datetime-local"
-          name="showing_time"
-          value={showing.showing_time}
-          onChange={handleChange}
-          className="admin-input"
-          required
-        />
-        <label className="admin-label">Adult Price:</label>
-        <input
-          type="number"
-          name="price_adult"
-          placeholder="Adult Price"
-          value={showing.price_adult}
-          onChange={handleChange}
-          className="admin-input"
-          required
-        />
-        <label className="admin-label">Child Price:</label>
-        <input
-          type="number"
-          name="price_child"
-          placeholder="Child Price"
-          value={showing.price_child}
-          onChange={handleChange}
-          className="admin-input"
-          required
-        />
-        <label className="admin-label">Senior Price:</label>
-        <input
-          type="number"
-          name="price_senior"
-          placeholder="Senior Price"
-          value={showing.price_senior}
-          onChange={handleChange}
-          className="admin-input"
-          required
-        />
-        <button type="submit" className="admin-button">
-          Add Showing
-        </button>
-      </form>
-    </div>
-    <div className='circle-one'></div>
-    <div className='circle-two'></div>
+      <Navbar />
+      <div className="admin-add-showing-container">
+        <h1 className="admin-title">Add New Showing</h1>
+        <form className="admin-form" onSubmit={handleSubmit}>
+          <select
+            name="movie_id"
+            value={showing.movie_id}
+            onChange={handleChange}
+            className="admin-select"
+            required
+          >
+            <option value="">Select Movie</option>
+            {movies.map((movie) => (
+              <option key={movie.movie_id} value={movie.movie_id}>
+                {movie.title}
+              </option>
+            ))}
+          </select>
+          <select
+            name="theater_id"
+            value={showing.theater_id}
+            onChange={handleChange}
+            className="admin-select"
+            required
+          >
+            <option value={1}>Theater 1</option>
+            <option value={2}>Theater 2</option>
+          </select>
+          <input
+            type="datetime-local"
+            name="showing_time"
+            value={showing.showing_time}
+            onChange={handleChange}
+            className="admin-input"
+            required
+          />
+          <label className="admin-label">Adult Price:</label>
+          <input
+            type="number"
+            name="price_adult"
+            placeholder="Adult Price"
+            value={showing.price_adult}
+            onChange={handleChange}
+            className="admin-input"
+            required
+          />
+          <label className="admin-label">Child Price:</label>
+          <input
+            type="number"
+            name="price_child"
+            placeholder="Child Price"
+            value={showing.price_child}
+            onChange={handleChange}
+            className="admin-input"
+            required
+          />
+          <label className="admin-label">Senior Price:</label>
+          <input
+            type="number"
+            name="price_senior"
+            placeholder="Senior Price"
+            value={showing.price_senior}
+            onChange={handleChange}
+            className="admin-input"
+            required
+          />
+          <button type="submit" className="admin-button">
+            Add Showing
+          </button>
+        </form>
+      </div>
+      <div className="circle-one"></div>
+      <div className="circle-two"></div>
     </>
   );
 };
