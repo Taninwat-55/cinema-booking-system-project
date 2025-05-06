6334825db8666ad699eeb5d106a1b6806f4e163b
Taninwat K. (Ice) <60184555+Taninwat-55@users.noreply.github.com>
2025-05-06 11:42:12 +0200

    Feature/claim guest booking (#117)
    * edit README file
    
    * feat: implement auto-claim of guest booking after register or login
    
    * edit title in index

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -5,7 +5,7 @@ function getUserByEmail(email) {
 }
 
 function createUser(email, password, name) {
-  return db
+  const result = db
     .prepare(
       `
     INSERT INTO users (email, password, name)
@@ -13,6 +13,12 @@ function createUser(email, password, name) {
   `
     )
     .run(email, password, name);
+
+  return db
+    .prepare(
+      'SELECT user_id, email, name, is_admin FROM users WHERE user_id = ?'
+    )
+    .get(result.lastInsertRowid);
 }
 
 module.exports = { getUserByEmail, createUser };
--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -7,6 +7,7 @@ const {
   getBookingByBookingNumber,
   trackBookingByNumber,
   cancelBookingById,
+  claimBooking
 } = require('../controllers/bookingController');
 
 const { requireAuth } = require('../middlewares/authMiddleware');
@@ -16,5 +17,6 @@ router.get('/user/:id', requireAuth, getBookingsByUserId);
 router.get('/confirmation/:bookingNumber', getBookingByBookingNumber);
 router.get('/track/:booking_number', trackBookingByNumber);
 router.put('/cancel/:id', requireAuth, cancelBookingById);
+router.patch('/claim', claimBooking);
 
 module.exports = router;
